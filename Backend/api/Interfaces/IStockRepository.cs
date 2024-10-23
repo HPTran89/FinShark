e8fe37721c676459c7601616bc1465ae9a277a23
Hung Tran <hungptran89@gmail.com>
2024-10-23 18:08:11 -0500

    Implemented Repo pattern for stock controller

---
+++ Backend/api/Interfaces/IStockRepository.cs
@@ -0,0 +1,17 @@
+﻿using api.DTOs.Stock;
+using api.Models;
+
+namespace api.Interfaces
+{
+    public interface IStockRepository
+    {
+        Task<List<Stock>> GetAllStocks();
+        Task<Stock?> GetStockById(int id);
+        Task<Stock> CreateStock(Stock stock);
+        Task<Stock?> GetStockBySymbol(string symbol);
+
+        Task<Stock> UpdateStock(Stock forUpdate, UpdateStockRequestDto updates);
+        Task DeleteStock(Stock forDelete);
+    }
+
+}
