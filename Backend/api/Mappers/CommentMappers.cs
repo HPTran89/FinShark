a6f4a621d18b78d5cbcb8b2501abc1305a5e465b
Hung Tran <hungptran89@gmail.com>
2024-12-03 14:11:49 -0600

    Added missing files

---
+++ Backend/api/Mappers/CommentMappers.cs
@@ -0,0 +1,41 @@
+﻿using api.DTOs.Comment;
+using api.Models;
+
+namespace api.Mappers
+{
+    public static class CommentMappers
+    {
+        public static CommentDto ToCommentDto(this Comment comment)
+        {
+            return new CommentDto
+            {
+                Id = comment.Id,
+                Title = comment.Title,
+                Content = comment.Content,
+                CreatedOn = comment.CreatedOn,
+                StockId = comment.StockId,
+                //Stock = comment.Stock.ToStockDto(),
+
+            };
+        }
+
+        public static Comment ToCommentFromCreate(this CreateCommentDto comment, int stockId)
+        {
+            return new Comment
+            {
+                Title = comment.Title,
+                Content = comment.Content,
+                StockId = stockId
+            };
+        }
+
+        public static Comment ToCommentFromUpdate(this UpdateCommentRequestDto comment)
+        {
+            return new Comment
+            {
+                Title = comment.Title,
+                Content = comment.Content
+            };
+        }
+    }
+}
